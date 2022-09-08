5673e62addb93b55bc5e43f968d63c3f6ede4aeb
Jonathan M.Bélec <88504898+jonathanmbelec@users.noreply.github.com>
2022-09-08 12:56:37 -0400

    Update Contribution breakdown quotidien.sql

--- Contribution breakdown quotidien.sql
+++ Contribution breakdown quotidien.sql
@@ -2,41 +2,40 @@
 Declare @DateTo date
 Set @DateTo = getdate()
 
-/*Tableau sommaire qui mentionne quotidiennement le nombre et le montant des contributeurs selon les catégories mentionnées*/
-SELECT ((date_format(convert_tz(date, '+00:00', 'US/Eastern'),'%Y-%m-%d'))) AS journée
-	   ,'Nouvelle contribution récurrente mensuelle' AS 'information extraite'
-	   , SUM(nb) AS quantité
+/*Tableau sommaire qui mentionne quotidiennement le nombre et le montant des contributeurs selon les catÃ©gories mentionnÃ©es*/
+SELECT ((date_format(convert_tz(date, '+00:00', 'US/Eastern'),'%Y-%m-%d'))) AS journÃ©e
+	   ,'Nouvelle contribution rÃ©currente mensuelle' AS 'information extraite'
+	   , SUM(nb) AS quantitÃ©
 	   , SUM(total) AS 'dollars' 
-FROM ( SELECT
-			date
-			, amount
-			, COUNT(DISTINCT id_customer) AS nb
-			, amount*COUNT(DISTINCT id_customer) AS total
-		FROM (SELECT DISTINCT LOWER(email) AS email
-					,a.id_customer
-					,c.creation_date AS date
-					,d.amount
-					,amount_refunded
-					,(CASE WHEN e.id_subscription_detail NOT LIKE '%unique%' THEN 'recurrent' ELSE 0 END) AS type
-					,e.status
-			   FROM sandbox.DIM_CUSTOMER a
-                    inner join sandbox.FACT_CHARGE b on a.id_customer=b.id_customer
-                    inner join sandbox.DIM_CHARGE_DETAIL c on b.id_charge_detail=c.id_charge_detail
-                    inner join sandbox.FACT_SUBSCRIPTION d on d.id_customer=b.id_customer
-                    inner join sandbox.DIM_SUBSCRIPTION_DETAIL e on (e.id_subscription_detail=d.id_subscription_detail and substr(e.creation_date,1,10)=substr(c.creation_date,1,10))
-               WHERE c.status = 'succeeded'
-                    AND CONVERT_TZ(c.creation_date, '+00:00','Us/Eastern') >= '2022-01-01'
-                    AND CONVERT_TZ(c.creation_date, '+00:00','Us/Eastern') < @DateTo 
-				) b
-		GROUP BY 1,2
-		) c 
+FROM ( SELECT date
+	      , amount
+	      , COUNT(DISTINCT id_customer) AS nb
+	      , amount*COUNT(DISTINCT id_customer) AS total
+       FROM (SELECT DISTINCT LOWER(email) AS email
+		    ,a.id_customer
+		    ,c.creation_date AS date
+		    ,d.amount
+		    ,amount_refunded
+		    ,(CASE WHEN e.id_subscription_detail NOT LIKE '%unique%' THEN 'recurrent' ELSE 0 END) AS type
+		    ,e.status
+	     FROM sandbox.DIM_CUSTOMER a
+             INNER JOIN sandbox.FACT_CHARGE b ON a.id_customer=b.id_customer
+	     INNER JOIN sandbox.DIM_CHARGE_DETAIL c ON b.id_charge_detail=c.id_charge_detail
+	     INNER JOIN sandbox.FACT_SUBSCRIPTION d ON d.id_customer=b.id_customer
+	     INNER JOIN sandbox.DIM_SUBSCRIPTION_DETAIL e ON (e.id_subscription_detail=d.id_subscription_detail AND SUBSTR(e.creation_date,1,10)=SUBSTR(c.creation_date,1,10))
+             WHERE c.status = 'succeeded'
+	     AND CONVERT_TZ(c.creation_date, '+00:00','Us/Eastern') >= '2022-01-01'
+	     AND CONVERT_TZ(c.creation_date, '+00:00','Us/Eastern') < @DateTo 
+	    ) b
+	GROUP BY 1,2
+     ) c 
 GROUP BY 1,2 
 
 UNION 
-/* Montant Encaissé par les contributeurs mensuels actifs*/
-SELECT DATE_FORMAT(CONVERT_TZ(date, '+00:00','US/Eastern'), '%Y-%m-%d') AS journée
-	   , 'Total Récurrent Encaissés'
-	   , 0 AS quantité
+/* Montant EncaissÃ© par les contributeurs mensuels actifs*/
+SELECT DATE_FORMAT(CONVERT_TZ(date, '+00:00','US/Eastern'), '%Y-%m-%d') AS journÃ©e
+	   , 'Total RÃ©current EncaissÃ©s'
+	   , 0 AS quantitÃ©
 	   , SUM(amount*nb) AS 'dollars'
 FROM (  SELECT
 		     amount
@@ -53,10 +52,10 @@ GROUP BY 1,2
 
 UNION
 
-/*Quantité de contributeur mensuel actif lié au montant encaissé précédemment*/
-SELECT (DATE_FORMAT(DATE_FORMAT(CONVERT_TZ(date, '+00:00', 'US/Eastern'), "%Y-%m-%d"), "%Y-%m-%d")) AS journée
+/*QuantitÃ© de contributeur mensuel actif liÃ© au montant encaissÃ© prÃ©cÃ©demment*/
+SELECT (DATE_FORMAT(DATE_FORMAT(CONVERT_TZ(date, '+00:00', 'US/Eastern'), "%Y-%m-%d"), "%Y-%m-%d")) AS journÃ©e
 		, 'Contributeur mensuel actif'
-		, nb_active AS quantité
+		, nb_active AS quantitÃ©
 		, 0 AS 'dollars'
 FROM ( SELECT
 			 date
@@ -70,98 +69,100 @@ GROUP BY 1,2
 Union
 
 /*Nouveaux Contributeurs Uniques*/
-SELECT (DATE_FORMAT(DATE_FORMAT(CONVERT_TZ(date, '+00:00', 'Us/Eastern'), "%Y-%m-%d"), "%Y-%m-%d")) AS journée
-		, 'Nouvelle Contribution Unique'
-		, SUM(nb_users) AS quantité
-		, SUM(total) AS 'dollars'
+SELECT (DATE_FORMAT(DATE_FORMAT(CONVERT_TZ(date, '+00:00', 'Us/Eastern'), "%Y-%m-%d"), "%Y-%m-%d")) AS journÃ©e
+	, 'Nouvelle Contribution Unique'
+	, SUM(nb_users) AS quantitÃ©
+	, SUM(total) AS 'dollars'
 FROM ( SELECT  email
-			   ,date
-			   , amount
-			   , COUNT(DISTINCT id_customer_stripe) AS nb_users
-			   , COUNT(DISTINCT id_customer_stripe)*amount AS total
-	   FROM ( SELECT DISTINCT LOWER(email) AS email 
-					 , a.id_customer_stripe
-					 , c.creation_date AS date
-					 , b.amount
+	       , date
+	       , amount
+	       , COUNT(DISTINCT id_customer_stripe) AS nb_users
+	       , COUNT(DISTINCT id_customer_stripe)*amount AS total
+       FROM ( SELECT DISTINCT LOWER(email) AS email 
+		     , a.id_customer_stripe
+		     , c.creation_date AS date
+		     , b.amount
               FROM sandbox.DIM_CUSTOMER a
               INNER JOIN sandbox.FACT_CHARGE b ON a.id_customer=b.id_customer
               INNER JOIN sandbox.DIM_CHARGE_DETAIL c ON b.id_charge_detail=c.id_charge_detail
               WHERE description LIKE 'Contribution unique' AND c.status = 'succeeded'
-			  AND CONVERT_TZ(c.creation_date, '+00:00', 'Us/Eastern') >= '2022-01-01'
+	      AND CONVERT_TZ(c.creation_date, '+00:00', 'Us/Eastern') >= '2022-01-01'
               AND CONVERT_TZ(c.creation_date, '+00:00', 'Us/Eastern') < @dateTo
               AND email NOT IN (SELECT DISTINCT email
-							    FROM sandbox.DIM_CUSTOMER 
-								WHERE CONVERT_TZ(creation_date, '+00:00', 'Us/Eastern') <= MAKEDATE(year(now()),1))
+			        FROM sandbox.DIM_CUSTOMER 
+				WHERE CONVERT_TZ(creation_date, '+00:00', 'Us/Eastern') <= MAKEDATE(year(now()),1))
              ) a
-		GROUP BY 1, 2
-	) a
+	GROUP BY 1, 2
+      ) a
 GROUP BY 1,2
 
 Union
-/* Anciens Contributeurs Uniques qui recommencen à contribuer*/
+/* Anciens Contributeurs Uniques qui recommencen Ã  contribuer*/
 
 SELECT(DATE_FORMAT(DATE_FORMAT(CONVERT_TZ(date, '+00:00', 'Us/Eastern'), "%Y-%m-%d"), "%Y-%m-%d")) AS date_fin
 	  , 'Renouvellement contribution unique'
-	  , SUM(nb_users) AS 'quantité'
+	  , SUM(nb_users) AS 'quantitÃ©'
 	  , SUM(total) AS 'dollars'
 FROM ( SELECT date
-			  , amount
-			  , COUNT(DISTINCT id_customer_stripe) AS nb_users
-			  , COUNT(DISTINCT id_customer_stripe)*amount AS total
-	   FROM ( SELECT DISTINCT LOWER(email) AS email
-					 , a.id_customer_stripe
-					 , c.creation_date AS date
-					 , b.amount
+	      , amount
+	      , COUNT(DISTINCT id_customer_stripe) AS nb_users
+	      , COUNT(DISTINCT id_customer_stripe)*amount AS total
+       FROM ( SELECT DISTINCT LOWER(email) AS email
+		     , a.id_customer_stripe
+		     , c.creation_date AS date
+		     , b.amount
               FROM sandbox.DIM_CUSTOMER a
               INNER JOIN sandbox.FACT_CHARGE b ON a.id_customer=b.id_customer
               INNER JOIN sandbox.DIM_CHARGE_DETAIL c ON b.id_charge_detail=c.id_charge_detail
               WHERE description LIKE 'Contribution unique' AND c.status = "succeeded"
-			  AND CONVERT_TZ(c.creation_date, "+00:00", "Us/Eastern")  >= '2022-01-01'
+	      AND CONVERT_TZ(c.creation_date, "+00:00", "Us/Eastern")  >= '2022-01-01'
               AND CONVERT_TZ(c.creation_date, "+00:00", "Us/Eastern") < @dateTo
               AND email IN (SELECT DISTINCT email
-							FROM sandbox.DIM_CUSTOMER
-							WHERE CONVERT_TZ(creation_date, '+00:00', 'Us/Eastern') <= MAKEDATE(year(now()),1))
-			) a
-		GROUP BY 1, 2
-	) a
+			    FROM sandbox.DIM_CUSTOMER
+			    WHERE CONVERT_TZ(creation_date, '+00:00', 'Us/Eastern') <= MAKEDATE(year(now()),1))
+	     ) a
+	GROUP BY 1, 2
+      ) a
 GROUP BY 1,2
 
 UNION
 /* Toutes les contributions uniques */
-SELECT (DATE_FORMAT(DATE_FORMAT(CONVERT_TZ(date, '+00:00', 'Us/Eastern'), "%Y-%m-%d"), "%Y-%m-%d")) AS journée
-		, 'Total contribution unique'
-		, SUM(nb_users) AS quantité
-		, SUM(total) AS 'dollars'
-FROM( SELECT date
-			 , amount
-			 , COUNT(DISTINCT id_customer_stripe) AS nb_users
-			 , COUNT(DISTINCT id_customer_stripe)*amount AS total
-	  FROM (  SELECT DISTINCT LOWER(email) AS email
-					 , a.id_customer_stripe
-					 , c.creation_date AS date
-					 , b.amount
-			  FROM sandbox.DIM_CUSTOMER a
-			  INNER JOIN sandbox.FACT_CHARGE b ON a.id_customer=b.id_customer
-			  INNER JOIN sandbox.DIM_CHARGE_DETAIL c ON b.id_charge_detail=c.id_charge_detail
-			  WHERE description LIKE 'Contribution unique' AND c.status = 'succeeded'
-              AND CONVERT_TZ(c.creation_date, '+00:00', 'Us/Eastern')  >= '2022-01-01'
-              AND CONVERT_TZ(c.creation_date, '+00:00', 'Us/Eastern')  < @dateTo ) a
-	group by 1,2) a		
-    group by 1,2
+SELECT (DATE_FORMAT(DATE_FORMAT(CONVERT_TZ(date, '+00:00', 'Us/Eastern'), "%Y-%m-%d"), "%Y-%m-%d")) AS journÃ©e
+	, 'Total contribution unique'
+	, SUM(nb_users) AS quantitÃ©
+	, SUM(total) AS 'dollars'
+FROM ( SELECT date
+	     , amount
+	     , COUNT(DISTINCT id_customer_stripe) AS nb_users
+	     , COUNT(DISTINCT id_customer_stripe)*amount AS total
+       FROM (  SELECT DISTINCT LOWER(email) AS email
+      		     , a.id_customer_stripe
+		     , c.creation_date AS date
+		     , b.amount
+	       FROM sandbox.DIM_CUSTOMER a
+	       INNER JOIN sandbox.FACT_CHARGE b ON a.id_customer=b.id_customer
+	       INNER JOIN sandbox.DIM_CHARGE_DETAIL c ON b.id_charge_detail=c.id_charge_detail
+	       WHERE description LIKE 'Contribution unique' AND c.status = 'succeeded'
+               AND CONVERT_TZ(c.creation_date, '+00:00', 'Us/Eastern')  >= '2022-01-01'
+               AND CONVERT_TZ(c.creation_date, '+00:00', 'Us/Eastern')  < @dateTo 
+	    ) a
+ 	GROUP BY 1,2
+      ) a	
+GROUP BY 1,2
 UNION
 
 /* Attrition*/
-SELECT (DATE_FORMAT(date, "%Y-%m-%d")) AS journée
-		, 'Attrition des récurrents'
-		, nb AS quantité
-		, dollars AS 'dollars'
-FROM (SELECT (DATE_FORMAT(canceled_at, "%Y-%m-%d")) AS date
-			 , COUNT(DISTINCT id) AS nb
-			 , SUM(amount) AS dollars
-	  FROM sandbox.CANCELLED_SUBSCRIPTION
-	  WHERE convert_tz(canceled_at, '+00:00', 'Us/Eastern') >= '2022-01-01'
-	  AND convert_tz(canceled_at, '+00:00', 'Us/Eastern') <@dateTo
-	  GROUP BY 1
-	 ) a
+SELECT (DATE_FORMAT(date, "%Y-%m-%d")) AS journÃ©e
+	, 'Attrition des rÃ©currents'
+	, nb AS quantitÃ©
+	, dollars AS 'dollars'
+FROM ( SELECT (DATE_FORMAT(canceled_at, "%Y-%m-%d")) AS date
+	     , COUNT(DISTINCT id) AS nb
+	     , SUM(amount) AS dollars
+       FROM sandbox.CANCELLED_SUBSCRIPTION
+       WHERE convert_tz(canceled_at, '+00:00', 'Us/Eastern') >= '2022-01-01'
+       AND convert_tz(canceled_at, '+00:00', 'Us/Eastern') <@dateTo
+       GROUP BY 1
+      ) a
 GROUP BY 1,2
 ;
